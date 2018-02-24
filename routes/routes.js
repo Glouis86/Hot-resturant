f1a56f523f34881913458f16c2349f5ca14150ed
Glouis86 <32691433+Glouis86@users.noreply.github.com>
2018-02-24 12:57:33 -0500

    updates

---
+++ routes/routes.js
@@ -0,0 +1,13 @@
+var tableData = require("../data/tableData");
+var waitListData = require("../data/waitinglistData");
+
+
+module.exports = function(app) {
+
+  app.get("/api/tables", function(req, res) {
+    res.json(tableData);
+  });
+
+  app.get("/api/waitlist", function(req, res) {
+    res.json(waitListData);
+  });
