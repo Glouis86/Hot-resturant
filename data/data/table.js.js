1e06e1c33ce61c01047bd87aaaf89a6a5e4edf4e
Glouis86 <32691433+Glouis86@users.noreply.github.com>
2018-02-24 12:32:29 -0500

    Add files via upload

---
+++ data/data/table.js.js
@@ -0,0 +1,11 @@
+var tableArray = [
+  {
+    customerName: "Amare",
+    customerEmail: "amare@example.com",
+    customerID: "aml89",
+    phoneNumber: "000-000-0000"
+  }
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = tableArray;
